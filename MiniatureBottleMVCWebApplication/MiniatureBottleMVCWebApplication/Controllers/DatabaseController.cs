3e1e66f5d0ec4369159c737df03d2e5b57a7ab21
Blagovest Zlatev <blagozlatev@gmail.com>
2013-02-05 17:38:51 +0200

    Add Database controller
    Add Database controller

---
+++ MiniatureBottleMVCWebApplication/MiniatureBottleMVCWebApplication/Controllers/DatabaseController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiniatureBottleMVCWebApplication.Models;
+
+namespace MiniatureBottleMVCWebApplication.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        private MiniatureBottleContext db = new MiniatureBottleContext();
+
+        //
+        // GET: /Database/
+
+        public ActionResult Index()
+        {
+            return View(db.Bottles.ToList());
+        }
+
+        //
+        // GET: /Database/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Bottle bottle = db.Bottles.Find(id);
+            if (bottle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bottle);
+        }
+
+        //
+        // GET: /Database/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Database/Create
+
+        [HttpPost]
+        public ActionResult Create(Bottle bottle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bottles.Add(bottle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bottle);
+        }
+
+        //
+        // GET: /Database/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Bottle bottle = db.Bottles.Find(id);
+            if (bottle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bottle);
+        }
+
+        //
+        // POST: /Database/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Bottle bottle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bottle).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bottle);
+        }
+
+        //
+        // GET: /Database/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Bottle bottle = db.Bottles.Find(id);
+            if (bottle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bottle);
+        }
+
+        //
+        // POST: /Database/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bottle bottle = db.Bottles.Find(id);
+            db.Bottles.Remove(bottle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MiniatureBottleMVCWebApplication/MiniatureBottleMVCWebApplication/Models/BottleModels.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MiniatureBottleMVCWebApplication.Models
+{
+    public class Bottle
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [StringLength(100)]
+        public string AlcoholType { get; set; }
+
+        [StringLength(100)]
+        public string Alcohol { get; set; }
+
+        [StringLength(100)]
+        public string Content { get; set; }
+
+        [Range(1, 300)]
+        public int Age { get; set; }
+
+        [StringLength(100)]
+        public string Shell { get; set; }
+
+        [Required,StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        public string Shape { get; set; }
+
+        [StringLength(100)]
+        public string Color { get; set; }
+
+        [StringLength(100)]
+        public string Material { get; set; }
+
+        [StringLength(100)]
+        public string Manufacturer { get; set; }
+
+        [StringLength(100)]
+        public string City { get; set; }
+
+        [StringLength(100)]
+        public string Country { get; set; }
+
+        [StringLength(50)]
+        public string Continent { get; set; }
+
+        [StringLength(255)]
+        public string Note { get; set; }
+    }
+
+    public class MiniatureBottleContext : DbContext
+    {
+        public DbSet<Bottle> Bottles { get; set; }
+    }
+}
