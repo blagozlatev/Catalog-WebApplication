e4fd98fc3f44394384d3d9e8dda4ff86e98f9ecb
Blagovest Zlatev <blagozlatev@gmail.com>
2013-02-06 16:51:10 +0200

    Insert from JSON put method.

--- MiniatureBottleMVCWebApplication/MiniatureBottleMVCWebApplication/Controllers/JSONController.cs
+++ MiniatureBottleMVCWebApplication/MiniatureBottleMVCWebApplication/Controllers/JSONController.cs
@@ -4,22 +4,42 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MiniatureBottleMVCWebApplication.Models;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace MiniatureBottleMVCWebApplication.Controllers
 {
     public class JSONController : Controller
     {
+        private MiniatureBottleContext context = new MiniatureBottleContext();
         //
         // GET: /JSON/
 
-        public JsonResult Index()
+        public JsonResult Index(int id = 0)
         {
-            List<Bottle> bottles;
-            using (MiniatureBottleContext context = new MiniatureBottleContext()){
-                bottles = context.Bottles.ToList();                
-            }            
-            return this.Json(bottles, JsonRequestBehavior.AllowGet);
+
+            if (id == 0)
+            {
+                List<Bottle> bottles = context.Bottles.ToList();
+                return this.Json(bottles, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                Bottle bottle = context.Bottles.Find(id);
+                return this.Json(bottle, JsonRequestBehavior.AllowGet);
+            }
+
         }
 
+        [HttpPut]
+        public void JSONPut()
+        {
+            Stream s = Request.InputStream;
+            StreamReader sr = new StreamReader(s);
+            string json = sr.ReadToEnd();
+            Bottle b = (Bottle)JsonConvert.DeserializeObject(json);
+            context.Bottles.Add(b);
+        }
     }
 }
