405b5ea76e2a05c236b27b363a74e38c742df104
Blagovest Zlatev <blagozlatev@gmail.com>
2013-02-13 22:15:10 +0200

    Added real serialization/deserialization
    Added real serialization/deserialization in the BottleModels.
    Respectfully the same functionality is added to the WPF Desktop App in
    the Bottle class.

--- MiniatureBottleMVCWebApplication/MiniatureBottleMVCWebApplication/Controllers/SerializedController.cs
+++ MiniatureBottleMVCWebApplication/MiniatureBottleMVCWebApplication/Controllers/SerializedController.cs
@@ -16,27 +16,20 @@ namespace MiniatureBottleMVCWebApplication.Controllers
         // GET: /Serialized/        
 
         public string Index(int id = 0)
-        {
-            string strReturn = string.Empty;
+        {            
             if (id == 0)
             {
                 List<Bottle> bottles = context.Bottles.ToList();
+                string strReturn = string.Empty;
                 foreach (Bottle b in bottles)
                 {
-                    strReturn += b.Age + "#" + b.Alcohol + "#" + b.AlcoholType + "#" +
-                        b.City + "#" + b.Color + "#" + b.Content + "#" + b.Continent + "#"
-                        + b.Country + "#" + b.ID + "#" + b.Manufacturer + "#" + b.Material
-                         + "#" + b.Name + "#" + b.Note + "#" + b.Shape + "#" + b.Shell + "\n";
+                    strReturn += b.Serialize();
                 }
                 return strReturn;
             }
             else
-            {
-                Bottle b = context.Bottles.Find(id);
-                return strReturn += b.Age + "#" + b.Alcohol + "#" + b.AlcoholType + "#" +
-                        b.City + "#" + b.Color + "#" + b.Content + "#" + b.Continent + "#"
-                        + b.Country + "#" + b.ID + "#" + b.Manufacturer + "#" + b.Material
-                         + "#" + b.Name + "#" + b.Note + "#" + b.Shape + "#" + b.Shell + "\n";
+            {                
+                return context.Bottles.Find(id).Serialize();
             }            
         }
 
@@ -45,31 +38,19 @@ namespace MiniatureBottleMVCWebApplication.Controllers
 
         [HttpPost]
         public string Post()
-        {
-            Bottle b = new Bottle();
+        {            
             Stream s = Request.InputStream;
             StreamReader sr = new StreamReader(s);
+            Bottle b = new Bottle();
             while (!sr.EndOfStream)
             {
-                string bottle = sr.ReadLine();
-                string[] btlArgs = bottle.Split('#');                
-                b.Age = int.Parse(btlArgs[0]);
-                b.Alcohol = btlArgs[1];
-                b.AlcoholType = btlArgs[2];
-                b.City = btlArgs[3];
-                b.Color = btlArgs[4];
-                b.Content = btlArgs[5];
-                b.Continent = btlArgs[6];
-                b.Country = btlArgs[7];
-                b.ID = int.Parse(btlArgs[8]);
-                b.Manufacturer = btlArgs[9];
-                b.Material = btlArgs[10];
-                b.Name = btlArgs[11];
-                b.Note = btlArgs[12];
-                b.Shape = btlArgs[13];
-                b.Shell = btlArgs[14];                
-                context.Bottles.Add(b);
-                context.SaveChanges();
+                string bottle = sr.ReadLine();                
+                b = Bottle.Deserialize(bottle);
+                if (b != null)
+                {
+                    context.Bottles.Add(b);
+                    context.SaveChanges();
+                }
             }            
             return b.ToString();
         }
--- MiniatureBottleMVCWebApplication/MiniatureBottleMVCWebApplication/Models/BottleModels.cs
+++ MiniatureBottleMVCWebApplication/MiniatureBottleMVCWebApplication/Models/BottleModels.cs
@@ -56,10 +56,58 @@ namespace MiniatureBottleMVCWebApplication.Models
 
         public override string ToString()
         {
+            string strReturn = "Age: " + this.Age + "\n" + "Alcohol: " + this.Alcohol + "\n" +
+                "Alcohol Type: " + this.AlcoholType + "\n" + "City: " + this.City + "\n" + "Color: " +
+                this.Color + "\n" + "Content: " + this.Content + "\n" + "Continent: " + this.Continent + "\n" +
+                "Country: " + this.Continent + "\n" + "ID: " + this.ID + "\n" + "Manufacturer: " +
+                this.Manufacturer + "\n" + "Material: " + this.Material + "\n" + "Name: " + this.Name + "\n" +
+                "Note: " + this.Note + "\n" + "Shape: " + this.Shape + "\n" + "Shell: " + this.Shell + "\n";
+            return strReturn;
+        }
+
+        public string Serialize()
+        {
+
             return this.Age + "#" + this.Alcohol + "#" + this.AlcoholType + "#" +
                         this.City + "#" + this.Color + "#" + this.Content + "#" + this.Continent + "#"
                         + this.Country + "#" + this.ID + "#" + this.Manufacturer + "#" + this.Material
-                         + "#" + this.Name + "#" + this.Note + "#" + this.Shape + "#" + this.Shell + "\n"; ;
+                         + "#" + this.Name + "#" + this.Note + "#" + this.Shape + "#" + this.Shell + "\n";
+        }        
+
+        public static Bottle Deserialize(string serialized)
+        {
+            string[] split = serialized.Split('#');
+            for (int i = 0; i < split.Count(); i++)
+            {
+                if (string.IsNullOrEmpty(split[i]))
+                {
+                    split[i] = string.Empty;
+                }
+            }
+            Bottle bottle = new Bottle();
+            try
+            {
+                bottle.Age = int.Parse(split[0]);
+                bottle.Alcohol = split[1];
+                bottle.AlcoholType = split[2];
+                bottle.City = split[3];
+                bottle.Color = split[4];
+                bottle.Content = split[5];
+                bottle.Continent = split[6];
+                bottle.Country = split[7];
+                bottle.ID = int.Parse(split[8]);
+                bottle.Manufacturer = split[9];
+                bottle.Material = split[10];
+                bottle.Name = split[11];
+                bottle.Note = split[12];
+                bottle.Shape = split[13];
+                bottle.Shell = split[14];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                return null;
+            }
+            return bottle;
         }
     }
 
